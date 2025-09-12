1d38089c6a296913434f7582350e2962673b518a
VirtualAngel <S224911721@deakin.edu.au>
2025-09-13 04:54:56 +1000

    Redeploy

--- server/newrelic.cjs
+++ server/newrelic.cjs
@@ -1,21 +1,21 @@
 'use strict'
+
+console.log('NR key:', process.env.NEW_RELIC_LICENSE_KEY)
+
 exports.config = {
   app_name: ['To-Do App'],
   license_key: process.env.NEW_RELIC_LICENSE_KEY,
   distributed_tracing: {
     enabled: true
   },
-
   logging: {
-    level: 'info'
+    level: 'trace'
   },
-
   instrumentation: {
     timers: {
       enabled: false
     }
   },
-
   allow_all_headers: true,
   attributes: {
     exclude: [
