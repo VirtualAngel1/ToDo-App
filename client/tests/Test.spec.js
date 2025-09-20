6bbc3bf93331c0ab3dddf14cbdb3eab9023ccb8a
VirtualAngel <S224911721@deakin.edu.au>
2025-09-20 17:35:13 +1000

    Redeploy

--- client/tests/Test.spec.js
+++ client/tests/Test.spec.js
@@ -2,5 +2,5 @@ const { test, expect } = require('@playwright/test');
 
 test('Home page loads', async ({ page }) => {
   await page.goto(process.env.SERVICE_URL_FRONTEND || 'https://todo-app-4g2e.onrender.com');
-  await expect(page).toHaveTitle(/ToDo/i);
+  await expect(page).toHaveTitle(/To-Do App/i);
 });
