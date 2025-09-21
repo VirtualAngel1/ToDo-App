354aca1f45e7136c143a37738b37c02aa7f643d7
VirtualAngel <S224911721@deakin.edu.au>
2025-09-21 16:46:46 +1000

    Redeploy

---
+++ server/src/main/java/com/example/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User loginRequest) {
+        Optional<User> user = userRepository.findByUsername(loginRequest.getUsername());
+        if (user.isPresent() && user.get().getPassword().equals(loginRequest.getPassword())) {
+            return ResponseEntity.ok("Login successful");
+        } else {
+            return ResponseEntity.status(401).body("Invalid credentials");
+        }
+    }
+}
