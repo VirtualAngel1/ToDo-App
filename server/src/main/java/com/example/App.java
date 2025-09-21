9e7fbaea6e5f624e068b97497e2132c0e2952b73
VirtualAngel <S224911721@deakin.edu.au>
2025-09-21 19:30:21 +1000

    Redeploy

--- server/src/main/java/com/example/App.java
+++ server/src/main/java/com/example/App.java
@@ -1,12 +1,23 @@
 package com.example;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
     public static void main(String[] args) {
         System.out.println("ToDo Server starting...");
         SpringApplication.run(App.class, args);
+    }
+
+    @PostConstruct
+    public void logMongoUri() {
+        System.out.println(">>> Mongo URI in use: " + mongoUri);
     }
 }
