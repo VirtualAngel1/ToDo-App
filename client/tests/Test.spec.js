502c7b257f4f47ff9cd3f9caa81e68bac3e466e7
VirtualAngel <S224911721@deakin.edu.au>
2025-09-30 20:34:52 +1000

    Redeploy

--- client/tests/Test.spec.js
+++ client/tests/Test.spec.js
@@ -1,6 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Home page loads', async ({ page }) => {
-  await page.goto(process.env.SERVICE_URL_FRONTEND || 'https://todo-app-4g2e.onrender.com');
+  console.log('Navigating to homepage...');
+  await page.goto('/', { timeout: 10000 });
+  console.log('Page loaded.');
   await expect(page).toHaveTitle(/To-Do App/i);
 });
